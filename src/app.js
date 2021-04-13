be789175b9aa419d0132645824c15a3f3f0459d0
Amit Alon <amit@kaholo.io>
2021-04-13 14:32:41 +0300

    added extra args param to begin method

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ async function execBegin(action, settings) {
     }
     const projName = (action.params.projectName || "").trim();
     const projVer = (action.params.projectVer || "").trim();
-    
+    const extraArgsStr = (action.params.args || "").trim();
 
     let args = [
         "begin", 
@@ -17,6 +17,10 @@ async function execBegin(action, settings) {
     ];
     if (projName) args.push(`/n:${projName}`);
     if (projVer) args.push(`/v:${projVer}`);
+    if (extraArgsStr) {
+        const extraArgs = extraArgsStr.split("\n").map(arg => arg.trim()).filter(arg => arg);
+        args = args.concat(extraArgs);
+    }
     return runSonarMsCmd(args, action, settings);
 }
 
